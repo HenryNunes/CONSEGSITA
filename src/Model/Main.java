15d1e6317b159a0a6e803c74c8e53cae7bfbdcd2
HenryNunes <henry.nunes@outlook.com>
2022-07-12 15:31:06 -0300

    Add files via upload

---
+++ src/Model/Main.java
@@ -0,0 +1,31 @@
+package Model;
+
+import TransformationPatterns.ITransform;
+import TransformationPatterns.NewCastleV1;
+
+public class Main {
+
+	public static void main(String[] args) { //APenas para teste
+		Row r = new Row();
+		//G.006;0; 427.84; 0.0; 53.0; 21.26; 0.0;20220630;000426
+		r.addValue("G.006");
+		r.addValue("0");
+		r.addValue("427.84");
+		r.addValue("0.0");
+		r.addValue("53.0");
+		r.addValue("21.26");
+		r.addValue("0.0");
+		r.addValue("20220630");
+		r.addValue("000426");
+		
+		ITransform trans = new NewCastleV1();
+		Row r2 = trans.transform(r, 3, 2, 4, 0);
+		
+		System.out.println(r2.getPos(0));
+		System.out.println(r2.getPos(1));
+		System.out.println(r2.getPos(2));
+
+		
+	}
+
+}
