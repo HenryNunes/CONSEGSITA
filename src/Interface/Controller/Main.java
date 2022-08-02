465dc5b6ccd646c72d679e1fb41dd1f27f6ba437
Luana Thomas <luana.thomas@hotmail.com>
2022-08-02 15:50:41 -0300

    adding files for experiments

--- src/Interface/Controller/Main.java
+++ src/Interface/Controller/Main.java
@@ -55,6 +55,8 @@ public class Main extends Application {
         facade.setT(t);
         facade.setA(a);
         String dest = sourceFileName.split("\\.")[0] + "_transformed." + sourceFileName.split("\\.")[1];
+        //String dest = "results\\sita" + s + i + t + a + "." + sourceFileName.split("\\.")[1];
+
         facade.transform(sourceFileName, dest, ";",s ,i ,t ,a);
         Main.updateProgress();
 
